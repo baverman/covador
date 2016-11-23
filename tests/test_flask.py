a4ee8612d38594f7009aaecf23fb58383ee6178e
Anton Bobrov <bobrov@vl.ru>
2016-11-23 16:56:21 +0300

    flask: json body support

--- tests/test_flask.py
+++ tests/test_flask.py
@@ -26,3 +26,15 @@ def test_get_form():
     request.get_data = staticmethod(lambda *args, **kwargs: b'boo=')
     assert repr(get_form()) == repr({'boo': [b'']})
     assert request._covador_form
+
+
+def test_json():
+    request.get_json = staticmethod(lambda *args, **kwargs: {'boo': 10})
+
+    @json_body(boo=int)
+    def test(boo):
+        assert boo == 10
+        test.called = True
+
+    test()
+    assert test.called
