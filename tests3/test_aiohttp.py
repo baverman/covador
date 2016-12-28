30469ba40e6a08174cbe975d298e9e790aef166e
Krivosheev Ivan <i.krivosheev@rambler-co.ru>
2016-12-28 15:39:52 +0300

    Fix problem with CBV in aiohttp.

--- tests3/test_aiohttp.py
+++ tests3/test_aiohttp.py
@@ -1,40 +1,58 @@
-import asyncio
+import pytest
 
-from covador.aiohttp import query_string, form
+from aiohttp import web
+from covador.aiohttp import form
+from covador.aiohttp import m_form
+from covador.aiohttp import query_string
+from covador.aiohttp import m_query_string
 
 
-def test_get_qs():
-    class Request(dict):
-        pass
-
-    request = Request()
-    request.query_string = 'boo=10'
+@pytest.fixture
+def cli(loop, test_client):
+    @form(boo=int)
+    async def hello_post(request, boo):
+        return web.Response(text='Hello, world {}'.format(boo))
 
     @query_string(boo=int)
-    @asyncio.coroutine
-    def handler(request, boo):
-        assert boo == 10
-        request.called = True
+    async def hello_get(request, boo):
+        return web.Response(text='Hello, world {}'.format(boo))
 
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(asyncio.wait_for(handler(request), 1))
-    assert request.called
+    class HelloView(web.View):
+
+        @m_query_string(boo=int)
+        async def get(self, boo):
+            return web.Response(text='Hello, world {}'.format(boo))
+
+        @m_form(boo=int)
+        async def post(self, boo):
+            return web.Response(text='Hello, world {}'.format(boo))
+
+    app = web.Application(loop=loop)
+    app.router.add_get('/', hello_get)
+    app.router.add_post('/', hello_post)
+    app.router.add_route('*', '/cbv/', HelloView)
+    return loop.run_until_complete(test_client(app))
 
 
-def test_get_form():
-    class Request(dict):
-        @asyncio.coroutine
-        def read(self):
-            return b'boo=10'
+async def test_get_qs(cli):
+    response = await cli.get('/', params={'boo': 5})
+    assert response.status == 200
+    assert await response.text() == 'Hello, world 5'
 
-    request = Request()
 
-    @form(boo=int)
-    @asyncio.coroutine
-    def handler(request, boo):
-        assert boo == 10
-        request.called = True
+async def test_get_form(cli):
+    response = await cli.post('/', data={'boo': 5})
+    assert response.status == 200
+    assert await response.text() == 'Hello, world 5'
 
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(asyncio.wait_for(handler(request), 1))
-    assert request.called
+
+async def test_m_query_string(cli):
+    response = await cli.get('/cbv/', params={'boo': 5})
+    assert response.status == 200
+    assert await response.text() == 'Hello, world 5'
+
+
+async def test_m_form(cli):
+    response = await cli.post('/cbv/', data={'boo': 5})
+    assert response.status == 200
+    assert await response.text() == 'Hello, world 5'
--- travis-install.sh
+++ travis-install.sh
@@ -2,4 +2,5 @@
 pip install pytest-cov==2.3.1
 if [[ $1 == 3* ]]; then
     pip install aiohttp
+    pip install pytest-aiohttp
 fi
