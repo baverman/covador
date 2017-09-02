c14397e9d9f82ab6a91adab6e28b4e978de5d777
Krivosheev Ivan <i.krivosheev@rambler-co.ru>
2017-09-02 20:49:49 +0300

    Fix install script

--- travis-install.sh
+++ travis-install.sh
@@ -2,5 +2,8 @@
 pip install pytest-cov==2.3.1 Flask tornado WebOb Django
 if [[ $1 == 3* ]]; then
     pip install aiohttp
-    pip install pytest-sanic
+    if [[ $1 == 3.5* ]]; then
+        pip install sanic
+        pip install pytest-sanic
+    fi
 fi
