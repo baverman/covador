55e244f40c3fbeced00302baeedfe2d3a9f93d53
Krivosheev Ivan <i.krivosheev@rambler-co.ru>
2017-09-02 21:37:49 +0300

    Fix travis scripts

--- travis-install.sh
+++ travis-install.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
+
+function version_gt() {
+    test "$(printf '%s\n' "$@" | sort -V | head -n 1)" != "$1";
+}
+
 pip install pytest-cov==2.3.1 Flask tornado WebOb Django
 if [[ $1 == 3* ]]; then
     pip install aiohttp
-    if [[ $1 == 3.[5-9]* ]]; then
+    if [[ version_gt $pyversion 3.4 ]]; then
         pip install sanic
         pip install pytest-sanic
     fi
--- travis-test.sh
+++ travis-test.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
+
+function version_gt() {
+    test "$(printf '%s\n' "$@" | sort -V | head -n 1)" != "$1";
+}
+
 pyversion=$1
 shift
-if [[ $pyversion == 3.[0-4]* ]]; then
-    py.test tests tests3 --cov covador --cov-report term-missing --cov-fail-under=100 --cov-config cov-py3.ini "$@"
-elif [[ $pyversion == 3.[5-9]*  ]]; then
+if [[ version_gt $pyversion 3.4 ]]; then
     py.test --cov covador --cov-report term-missing --cov-fail-under=100 --cov-config cov-py3.ini "$@"
+elif [[ $pyversion == 3*  ]]; then
+    py.test tests tests3 --cov covador --cov-report term-missing --cov-fail-under=100 --cov-config cov-py3.ini "$@"
 else
     py.test tests --cov covador --cov-report term-missing --cov-fail-under=100 --cov-config cov-py2.ini "$@"
 fi
