8b981025574ac15da550b431d46b5940e3b889ed
Krivosheev Ivan <i.krivosheev@rambler-co.ru>
2017-09-02 21:22:06 +0300

    Fix work with tests

--- travis-install.sh
+++ travis-install.sh
@@ -2,7 +2,7 @@
 pip install pytest-cov==2.3.1 Flask tornado WebOb Django
 if [[ $1 == 3* ]]; then
     pip install aiohttp
-    if [[ $1 == 3.5* ]]; then
+    if [[ $1 == 3.[5-9]* ]]; then
         pip install sanic
         pip install pytest-sanic
     fi
--- travis-test.sh
+++ travis-test.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 pyversion=$1
 shift
-if [[ $pyversion == 3* ]]; then
+if [[ $pyversion == 3.[0-4]* ]]; then
+    py.test tests tests3 --cov covador --cov-report term-missing --cov-fail-under=100 --cov-config cov-py3.ini "$@"
+elif [[ $pyversion == 3.[5-9]*  ]]; then
     py.test --cov covador --cov-report term-missing --cov-fail-under=100 --cov-config cov-py3.ini "$@"
 else
     py.test tests --cov covador --cov-report term-missing --cov-fail-under=100 --cov-config cov-py2.ini "$@"
