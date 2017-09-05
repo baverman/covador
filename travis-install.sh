0287568b2c3f3ac4733926a322880d5d1dd67e87
Krivosheev Ivan <i.krivosheev@rambler-co.ru>
2017-09-05 20:54:40 +0300

    Fix tests run

--- travis-install.sh
+++ travis-install.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 
-function version_gt() {
-    test "$(printf '%s\n' "$@" | sort -V | head -n 1)" != "$1";
-}
-
 pip install pytest-cov==2.3.1 Flask tornado WebOb Django
 if [[ $1 == 3* ]]; then
     pip install aiohttp
-    if version_gt $1 3.4; then
+    if [[ $1 != "3.4" ]]; then
         pip install sanic
         pip install pytest-sanic
     fi
--- travis-test.sh
+++ travis-test.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
 
-function version_gt() {
-    test "$(printf '%s\n' "$@" | sort -V | head -n 1)" != "$1";
-}
-
 pyversion=$1
 shift
-if version_gt $pyversion 3.4; then
-    py.test --cov covador --cov-report term-missing --cov-fail-under=100 --cov-config cov-py_3.4.ini "$@"
-elif [[ $pyversion == 3*  ]]; then
+if [[ $pyversion == "2.7" ]]; then
+    py.test tests --cov covador --cov-report term-missing --cov-fail-under=100 --cov-config cov-py2.ini "$@"
+elif [[ $pyversion == "3.4" ]]; then
     py.test tests tests3 --cov covador --cov-report term-missing --cov-fail-under=100 --cov-config cov-py3.ini "$@"
 else
-    py.test tests --cov covador --cov-report term-missing --cov-fail-under=100 --cov-config cov-py2.ini "$@"
+    py.test --cov covador --cov-report term-missing --cov-fail-under=100 --cov-config cov-py_3.4.ini "$@"
 fi
