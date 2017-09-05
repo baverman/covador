87973ed300348d05d4d42a623ed7a59936a018d6
Krivosheev Ivan <i.krivosheev@rambler-co.ru>
2017-09-05 20:58:07 +0300

    Fix tests run

--- travis-test.sh
+++ travis-test.sh
@@ -2,10 +2,10 @@
 
 pyversion=$1
 shift
-if [[ $pyversion == "2.7" ]]; then
+if [[ $pyversion == "2.7" || $pyversion == "pypy" ]]; then
     py.test tests --cov covador --cov-report term-missing --cov-fail-under=100 --cov-config cov-py2.ini "$@"
 elif [[ $pyversion == "3.4" ]]; then
-    py.test tests tests3 --cov covador --cov-report term-missing --cov-fail-under=100 --cov-config cov-py3.ini "$@"
+    py.test tests tests3.4 --cov covador --cov-report term-missing --cov-fail-under=100 --cov-config cov-py3.4.ini "$@"
 else
-    py.test --cov covador --cov-report term-missing --cov-fail-under=100 --cov-config cov-py_3.4.ini "$@"
+    py.test --cov covador --cov-report term-missing --cov-fail-under=100 --cov-config cov-py3.ini "$@"
 fi
